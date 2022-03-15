f8872b7b796abd5d2b49630238ae357ca1e50e51
Khateeb1111 <junaydkr@hotmail.com>
2022-03-15 16:43:32 +0000

    first

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  const logo = {
+    name: 'Fighters Central logo',
+    image: 'https://www.creativefabrica.com/wp-content/uploads/2019/02/Monogram-FC-Logo-by-Greenlines-Studios-580x387.jpg'
+  }
+  return (
+    <div className="App">
+      <header>
+      <nav>
+        (Logo) Home About Latest News Fighters Upcoming Fights Results Search Contact   
+      </nav>
+      </header>
+      <main>
+      
+      </main>
+    </div>
+  );
+}
+
+export default App;
